9c933959bc6b4acf4ed04d974d2385774b56b3ae
AmiraSayedMohamed <amira.sayedza@gmail.com>
2025-03-07 00:26:35 +0200

    Deleted all existing files

--- gui_code/measurement/measurement.py
+++
@@ -1,5 +0,0 @@
-print("Measurement module loaded")
-
-def run_measurement():
-    print("Running Measurement Mission")
-    # Add your measurement logic here
--- gui_code/modeling/modeling.py
+++
@@ -1,5 +0,0 @@
-print("Modeling module loaded")
-
-def run_modeling():
-    print("Running 3D Modeling Mission")
-    # Add your 3D modeling logic here
